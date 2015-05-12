d87118df3299c569bef4b5e1561f9ca8060d8837
Shengquan <shengquan.tuo@cern.ch>
2015-05-12 22:53:46 +0200

    tool to create storage-dumps

---
+++ SiteInfoProviders/LStore/lstoredump.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+begintime=`date +%s`
+OUTFILE="storage_dump_vandyT2.$begintime.txt"
+touch $OUTFILE
+echo "BEGIN TIME $begintime `date`" > $OUTFILE.log
+
+setpkgs -a lio
+lio_getattr -rd 1000 -new_obj "%s|" -attr_fmt "%.0s%s" -attr_sep "|" -al system.exnode.size,system.create,user.gridftp.adler32 '@:/cms/*' | sed -e 's/(null)//g' >> $OUTFILE
+
+echo "END TIME `date +%s` `date`" >> $OUTFILE.log 
